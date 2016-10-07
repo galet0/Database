233876a407d16a81181609d20c1d79c5c25d0199
galet0 <galeto.miteva@gmail.com>
2016-10-07 12:59:51 +0300

    Update 12. Rich Wizard, Poor Wizard.sql

--- DB_Homeworks/Data Aggregation/12. Rich Wizard, Poor Wizard.sql
+++ DB_Homeworks/Data Aggregation/12. Rich Wizard, Poor Wizard.sql
@@ -4,5 +4,9 @@ USE gringotts;
 If a wizard is the last one in the database, simply ignore it. 
 At the end you have to sum the difference between the deposits. */
 
-SELECT w.deposit_amount 
-  FROM wizzard_deposits AS w
+SELECT SUM(result.diff) AS sum_difference
+  FROM (SELECT (w.deposit_amount - next_row.deposit_amount) AS diff
+   		 FROM wizzard_deposits AS w
+  JOIN (SELECT w.id, w.deposit_amount 
+  			 FROM wizzard_deposits AS w) AS next_row
+  	 ON w.id + 1 = next_row.id) AS result
